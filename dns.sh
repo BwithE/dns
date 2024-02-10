36414a900a5a6259049f1b460e32719f81eb2beb
BwithE <144924113+BwithE@users.noreply.github.com>
2024-02-09 23:29:41 -0500

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -27,7 +27,7 @@ sudo tee /var/www/html/index.html > /dev/null <<EOT
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Matrix Effect</title>
+    <title>Access Denied</title>
     <style>
         body {
             background-color: black;
